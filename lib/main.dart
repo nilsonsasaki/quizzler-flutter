75cd71c6d0554add49b5e483e6256d7947c40cf5
nilsonsasaki <nilsonsasaki@gmail.com>
2021-04-07 20:15:09 -0300

    changed text to show question[0]

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class _QuizPageState extends State<QuizPage> {
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.'
   ];
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
